1b460deb86c445d1b87a3ad8507978e9c3977ad8
Jason Wilson <jason@wilsons.io>
2019-02-21 16:54:44 +0100

    feat(About): Update About Page

--- packages/photo-sans-client/src/components/about/about.js
+++ packages/photo-sans-client/src/components/about/about.js
@@ -5,8 +5,15 @@ class About extends Component {
   render() {
     return (
       <main className="about">
-        <h2 className="about__title">About</h2>
-        <p>Lorem ipsum is hard.</p>
+        <h2 className="about__title">Hi, I'm Jason,</h2>
+        <p>
+          I've never been good with cameras, especially not for night projects,
+          but I have been fortunate enough to see so many beautiful and
+          interesting places from beautiful Swiss mountainscapes to the solitude
+          of the Pacific Ocean. Photographer Sans is going to be my place to try
+          and convey the beauty of my world through words&mdash;a photographer
+          sans camera.
+        </p>
       </main>
     );
   }

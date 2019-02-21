c135be02ea42065a4a1a02cd8fe84c74b403268e
Jason Wilson <jason@wilsons.io>
2019-02-21 16:31:08 +0100

    feat(App): Add components to App

--- packages/photo-sans-client/src/App.js
+++ packages/photo-sans-client/src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
+import Header from "./components/header/header";
+import Nav from "./components/nav/nav";
+import Gallery from "./components/gallery/gallery";
+import About from "./components/about/about";
+import Footer from "./components/footer/footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>Well hello there.</p>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Nav />
+          <Route exact path="/" component={Gallery} />
+          <Route exact path="/about" component={About} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }

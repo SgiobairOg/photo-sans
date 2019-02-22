c9cc35df1995a31675973782eb9276b75370f13a
Jason Wilson <jason@wilsons.io>
2019-02-22 21:18:29 +0100

    feat(Gallery): Add Photo COmponents to Gallery

--- packages/photo-sans-client/src/App.js
+++ packages/photo-sans-client/src/App.js
@@ -6,19 +6,31 @@ import Gallery from "./components/gallery/gallery";
 import About from "./components/about/about";
 import Footer from "./components/footer/footer";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const graphURI = process.env.GRAPHQL_URI || "//localhost:5000/graphql?";
+const client = new ApolloClient({
+  uri: graphURI,
+  fetchOptions: {
+    contentType: "application/json"
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Nav />
-          <Route exact path="/" component={Gallery} />
-          <Route exact path="/about" component={About} />
-          <Footer />
-        </div>
-      </Router>
+      <ApolloProvider client={client}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Nav />
+            <Route exact path="/" component={Gallery} />
+            <Route exact path="/about" component={About} />
+            <Footer />
+          </div>
+        </Router>
+      </ApolloProvider>
     );
   }
 }
--- packages/photo-sans-client/src/components/gallery/gallery.js
+++ packages/photo-sans-client/src/components/gallery/gallery.js
@@ -1,12 +1,116 @@
 import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import * as merge from "deepmerge";
+import InfiniteScroll from "react-infinite-scroller";
+import Photo from "./../photo/photo";
 import "./gallery.css";
 
 class Gallery extends Component {
+  canLoadMore = true;
+  initialLoad = false;
+  lastCursor = "";
+
+  loadOlderPhotos = () => false;
+
   render() {
     return (
-      <main className="gallery">
-        <p>Gallery</p>
-      </main>
+      <Query
+        query={gql`
+          {
+            allPhotos(first: 2, orderBy: CREATED_DESC) {
+              totalCount
+              pageInfo {
+                endCursor
+              }
+              nodes {
+                id
+                title
+                body
+                created
+              }
+            }
+          }
+        `}
+      >
+        {({ loading, error, data: { allPhotos }, fetchMore }) => {
+          if (loading) {
+            return <p>Loading...</p>;
+          }
+          if (error) {
+            const errorString = JSON.stringify(error);
+            return <p>Error: {errorString}</p>;
+          }
+
+          console.log(allPhotos);
+          const { nodes, totalCount, pageInfo } = allPhotos;
+          console.log("Last Cursor:", pageInfo.endCursor);
+          this.canLoadMore = nodes.length < totalCount;
+          this.lastCursor = pageInfo.endCursor;
+
+          this.loadOlderPhotos = () => {
+            return fetchMore({
+              query: gql`
+                {
+                allPhotos(after: "${
+                  this.lastCursor
+                }" first: 2 orderBy: CREATED_DESC) {
+                  totalCount
+                  pageInfo {
+                    endCursor
+                  }
+                  nodes {
+                    id
+                    title
+                    body
+                    created
+                  }
+                }
+              }
+              `,
+              updateQuery: (prev, { fetchMoreResult }) => {
+                if (fetchMoreResult.allPhotos.pageInfo.endCursor === null) {
+                  this.canLoadMore = false;
+                  return prev;
+                }
+
+                return merge.all([
+                  {},
+                  prev,
+                  {
+                    allPhotos: {
+                      pageInfo: {
+                        endCursor: fetchMoreResult.allPhotos.pageInfo.endCursor
+                      },
+                      nodes: [...fetchMoreResult.allPhotos.nodes]
+                    }
+                  }
+                ]);
+              }
+            });
+          };
+
+          return (
+            <main className="gallery">
+              {allPhotos.nodes.map(photo => (
+                <Photo key={photo.id} photo={photo} />
+              ))}
+              {this.canLoadMore ? (
+                <button
+                  className="gallery__load-more"
+                  onClick={() => this.loadOlderPhotos()}
+                >
+                  Load Older Photos
+                </button>
+              ) : (
+                <p className="gallery__the-end">
+                  That was the very last one... I'll work on some more.
+                </p>
+              )}
+            </main>
+          );
+        }}
+      </Query>
     );
   }
 }

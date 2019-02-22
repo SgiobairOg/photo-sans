73b4be6b480e4e751fc422a2730588e4964d05a6
Jason Wilson <jason@wilsons.io>
2019-02-22 21:21:16 +0100

    fix(): Clear out Unused Code

--- packages/photo-sans-client/src/components/gallery/gallery.js
+++ packages/photo-sans-client/src/components/gallery/gallery.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import * as merge from "deepmerge";
-import InfiniteScroll from "react-infinite-scroller";
 import Photo from "./../photo/photo";
 import "./gallery.css";
 
--- packages/photo-sans-client/src/services/photos/photo.service.js
+++
@@ -1,8 +0,0 @@
-import Utils from "./../../assets/utils";
-class PhotosService {
-  getAllPhotos() {
-    console.log("Standing by");
-  }
-}
-const service = new PhotosService();
-export default service;

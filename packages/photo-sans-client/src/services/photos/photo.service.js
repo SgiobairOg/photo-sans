b6b14cc5751ac1f754dc433548a084b6d6bd81b2
Jason Wilson <jason@wilsons.io>
2019-02-22 21:18:00 +0100

    feat(Photos): Add Photo Service

---
+++ packages/photo-sans-client/src/services/photos/photo.service.js
@@ -0,0 +1,8 @@
+import Utils from "./../../assets/utils";
+class PhotosService {
+  getAllPhotos() {
+    console.log("Standing by");
+  }
+}
+const service = new PhotosService();
+export default service;
